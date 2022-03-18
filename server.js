1ca3d7520e45c0fe9e8966efd77d4a24a89203a9
CallmeVeeTech <vsunday.1710@gmail.com>
2022-03-18 11:33:58 +0100

    Set05 API

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const port = process.env.PORT || 2022
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req,res)=>{
+    res.send('My api is ready for consumption')
+})
+
+app.listen(port, ()=>{
+    console.log(`server is listening to ${port}`)
+})
